2bf3c93196bc05c8f1283958f18c6c942607cff6
chanyoung <kipa1003@naver.com>
2023-04-25 17:23:52 +0900

    feat: Image Cropper 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useState } from "react";
+import ImageCropper from "./components/ImageCropper";
 
 function App() {
-  return <div className="App"></div>;
+  const [uploadImage, setUploadImage] = useState("");
+  const handleUploadImage = (image: string) => setUploadImage(image);
+
+  return (
+    <div className="App">
+      <ImageCropper aspectRatio={1 / 1} onCrop={handleUploadImage}>
+        <button>이미지 업로드</button>
+      </ImageCropper>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ImageCropper.tsx
+++ src/components/ImageCropper.tsx
@@ -1,5 +1,86 @@
-const ImageCropper = () => {
-  return <div>ImageCropper</div>;
+import { useRef, useState, createRef } from "react";
+import { Cropper, ReactCropperElement } from "react-cropper";
+import "cropperjs/dist/cropper.css";
+
+interface PropsType {
+  onCrop: (image: string) => void;
+  aspectRatio: number;
+  children: React.ReactNode;
+}
+
+const ImageCropper = ({ children, aspectRatio, onCrop }: PropsType) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState<null | string>(null);
+  const cropperRef = useRef<ReactCropperElement>(null);
+
+  const handleChildrenClick = () => {
+    if (inputRef.current) inputRef.current.click();
+  };
+
+  const handleFileChange = (e: any) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result as string);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
+  const getCropData = () => {
+    if (typeof cropperRef.current?.cropper !== "undefined") {
+      onCrop(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+      setImage(null);
+    }
+  };
+
+  return (
+    <>
+      <input
+        type="file"
+        ref={inputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+      <div onClick={handleChildrenClick}>{children}</div>
+      {image && (
+        <div className="container">
+          <div className="backdrop" />
+          <div className="modal">
+            <h3>이미지 편집하기</h3>
+            <div className="content-wrapper">
+              <div className="content">
+                <Cropper
+                  ref={cropperRef}
+                  aspectRatio={aspectRatio}
+                  src={image}
+                  viewMode={1}
+                  width={800}
+                  height={500}
+                  background={false}
+                  responsive
+                  autoCropArea={1}
+                  checkOrientation={false}
+                  guides
+                />
+              </div>
+            </div>
+            <div className="footer">
+              <button onClick={() => setImage(null)}>취소</button>
+              <button className="crop" onClick={getCropData}>
+                적용하기
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ImageCropper;
